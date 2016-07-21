4a53748f866b06f5ffc79b7b8b7a6154d8168a04
ediblecode <ian.routledge@nice.org.uk>
2016-07-21 17:13:44 +0100

    Update sass doc templates and refactor colours

--- web/server/controllers/sass.js
+++ web/server/controllers/sass.js
@@ -1,13 +1,15 @@
+/*eslint-env node*/
 var express = require("express"),
 	router = express.Router(),
-	fs = require("fs");
+	fs = require("fs"),
+	path = require("path");
 
 router.get("/", function(req, res) {
 	res.render("sass/index");
 });
 
 router.get("/docs", function(req, res) {
-	var data = require("./../../../dist/docs/sass/sass.json");
+	var data = JSON.parse(fs.readFileSync(path.join(__dirname, "./../../../dist/docs/sass/sass.json"), "utf-8"));
 	res.render("sass/docs", data);
 });
 
