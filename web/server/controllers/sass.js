3afd224bfc9c05f2f00c30b582116b0e5d9f4793
ediblecode <ian.routledge@nice.org.uk>
2016-07-21 11:21:41 +0100

    Setup typography, borrowed heavily from sassline

--- web/server/controllers/sass.js
+++ web/server/controllers/sass.js
@@ -2,13 +2,12 @@ var express = require("express"),
 	router = express.Router(),
 	fs = require("fs");
 
-var data = require("./../../../dist/docs/sass/sass.json");
-
 router.get("/", function(req, res) {
 	res.render("sass/index");
 });
 
 router.get("/docs", function(req, res) {
+	var data = require("./../../../dist/docs/sass/sass.json");
 	res.render("sass/docs", data);
 });
 
