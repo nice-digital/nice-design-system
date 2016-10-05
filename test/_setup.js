69fdf00af3cd23d6a3cc6cab17c9edb7bdae9c97
ediblecode <ian.routledge@nice.org.uk>
2016-10-05 13:54:43 +0100

    Add chai promise plugin

--- test/_setup.js
+++ test/_setup.js
@@ -9,12 +9,16 @@ global.chai = require("chai");
 global.should = require("chai").should();
 global.AssertionError = require("chai").AssertionError;
 
+// Promises - see http://chaijs.com/plugins/chai-as-promised/
+chai.use(require("chai-as-promised"));
+
 // Use sinon for spying etc
 global.sinon = require("sinon");
-var sinonChai = require("sinon-chai");
-global.chai.use(sinonChai);
+global.chai.use(require("sinon-chai"));
 
 // Setup jsdom for faking dom and jquery
 global.document = require("jsdom").jsdom("<html><head></head><body></body></html>");
 global.window = document.defaultView;
 global.jQuery = global.$ = require("jquery");
+
+global.PRODUCTION = false;
