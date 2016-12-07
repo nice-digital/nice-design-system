bac10e88a8732cf5d4914635bb93ff91c8c68e81
ediblecode <ian.routledge@nice.org.uk>
2016-12-07 10:29:33 +0000

    Update post css task to add rem fallbacks properly for IE8

--- .grunt-tasks/postcss.js
+++ .grunt-tasks/postcss.js
@@ -2,7 +2,7 @@ module.exports = {
 	options: {
 		map: true,
 		processors: [
-			require("pixrem")(), // add fallbacks for rem units for IE8+
+			require("pixrem")({ html: false, atrules: true }), // add fallbacks for rem units for IE8+
 			require("autoprefixer")({ browsers: "> 1%, last 2 versions, ie >= 8" }), // add vendor prefixes
 		]
 	},
