8d8df09ee7559e12b7aad77e9d0a0a06fc51d728
ediblecode <ian.routledge@nice.org.uk>
2017-07-07 14:48:28 +0100

    Release v0.2.18

--- dist/javascripts/nice.oldie.dev.js
+++ dist/javascripts/nice.oldie.dev.js
@@ -1,5 +1,5 @@
 /*!
-NICE Design System 0.2.17 | 2017-06-30
+NICE Design System 0.2.18 | 2017-07-07
 © Copyright NICE 2015-2017
 Licensed under MIT (https://github.com/nhsevidence/nice-design-system/blob/master/LICENSE)
 */
