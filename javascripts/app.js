064b1247a0ac306bbedb8221dcab7b4fe2305053
ediblecode <ian.routledge@nice.org.uk>
2017-08-03 09:50:27 +0100

    Add es5 shim and anchor as conditional comment for ie9+

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,11 +1,15 @@
-var anchors = new AnchorJS();
 
-anchors.options = {
-	placement: "left",
-	visible: "touch"
-};
-anchors.add("[role='main'] h2, [role='main'] h3, [role='main'] h4");
-anchors.remove(".example h2, .example h3, .example h4, [data-no-anchor] h2, [data-no-anchor] h3");
+// IE8 doesn't support AnchorJS
+if(typeof AnchorJS === "function") {
+	var anchors = new AnchorJS();
+
+	anchors.options = {
+		placement: "left",
+		visible: "touch"
+	};
+	anchors.add("[role='main'] h2, [role='main'] h3, [role='main'] h4");
+	anchors.remove(".example h2, .example h3, .example h4, [data-no-anchor] h2, [data-no-anchor] h3");
+}
 
 $(document).ready(function() {
 
