ffbbc6a06592863e0f950a89e886f1bfbd4a1739
ediblecode <ian.routledge@nice.org.uk>
2017-12-19 11:53:07 +0000

    Update icons in readme to use a table
    Includes HTML, SASS and unicode codepoint

--- .grunt-tasks/replace.js
+++ .grunt-tasks/replace.js
@@ -1,4 +1,6 @@
-const regex = /<!-- START icons.*-->([\s\S]*)<!-- END icons/gm;
+const regex = /<!-- START icons.*-->([\s\S]*)<!-- END icons .*-->/gm,
+	startComment = "<!-- START icons generated comment -->\r\n<!-- DON'T EDIT THIS SECTION, INSTEAD RE-RUN COMMAND TO UPDATE -->\r\n\r\n",
+	endComment = "\r\n<!-- END icons generated comment -->";
 
 module.exports = {
 	options: {
@@ -9,23 +11,25 @@ module.exports = {
 				{
 					match: regex,
 					replacement: function(match, p1) {
-						console.log(font);
 
 						var font = require("./../dist/nice-icons.json");
 
-						var icons = "";
+						var iconTable = "Icon | Name | Unicode | HTML | SASS \r\n";
+						iconTable += "---- | ---- | ---- | ---- | ---- \r\n";
 
+						// Turn list of icons into markdown table
 						for (var i = 0; i < font.glyphs.length; i++) {
 							var glyph = font.glyphs[i],
-								file = font.files[i];
+								file = font.files[i],
+								codepoint = font.codepoints[i],
+								image = `<img src="${ file }" alt="${ glyph }" height="50">`,
+								html = `\`<span class="icon icon--${ glyph }" aria-hidden="true"></span>\``,
+								sass = `\`@include nice-icon(${ glyph });\``;
 
-							icons += `- <img src="${ file }" alt="${ glyph }" height="50"> ${ glyph } \r\n`;
+							iconTable += `${ image } | ${ glyph } | ${ codepoint } | ${ html } | ${ sass } \r\n`;
 						}
 
-						console.log(icons);
-
-
-						return `<!-- START icons -->\r\n${ icons }\r\n<!-- END icons`;
+						return `${ startComment }${ iconTable }${ endComment }`;
 					}
 				}
 			]
