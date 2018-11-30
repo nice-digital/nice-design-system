fe2ce7a76d81e0b54fb32080ed68a2591d506ec8
Ian Routledge <ian.routledge@nice.org.uk>
2018-11-30 15:04:21 +0000

    Add first lerna package for core

---
+++ packages/nds-core/__tests__/nds-core.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const ndsCore = require('..');
+
+describe('@nice-digital/nds-core', () => {
+    it('needs tests');
+});
---
+++ packages/nds-core/lib/nds-core.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = ndsCore;
+
+function ndsCore() {
+    // TODO
+}
