5463cb5b79d2ab4209a2ef7ff26031bbfa7a07b5
Ian Routledge <ian.routledge@nice.org.uk>
2018-12-04 10:37:01 +0000

    Add some action banner stories

--- packages/nds-action-banner/src/ActionBanner.js
+++ packages/nds-action-banner/src/ActionBanner.js
@@ -3,13 +3,46 @@ import React, { Component } from "react";
 import "../scss/action-banner.scss";
 
 type ActionBannerProps = {
-	title: string
+	title: string,
+	modifier: "subtle",
+	children: *,
+	cta: Element,
+	closeable: boolean
 };
 
-export default class ActionBanner extends Component<ActionBannerProps> {
-	render() {
-		return <div className="action-banner">
-			action banner
-		</div>;
-	}
-}
+const ActionBanner = (props: ActionBannerProps) => {
+
+	let classes = ["action-banner"];
+
+	if(props.modifier)
+		classes.push(`action-banner--${props.modifier}`);
+
+	if(props.closeable)
+		classes.push("action-banner--closeable");
+
+	return <section className={classes.join(" ")}>
+		<div className="action-banner__container">
+			<div className="action-banner__inner">
+				<div className="action-banner__text">
+					<h2 className="action-banner__title">
+						{props.title}
+					</h2>
+					<p className="action-banner__intro">
+						{props.children}
+					</p>
+				</div>
+				<div className="action-banner__actions">
+					{props.cta}
+				</div>
+				{props.closeable &&
+					<button type="button" className="action-banner__close">
+						<span className="icon icon--remove" ara-hidden="true"></span>
+						<span className="visually-hidden">Close</span>
+					</button>
+				}
+			</div>
+		</div>
+	</section>;
+};
+
+export default ActionBanner;
--- packages/nds-action-banner/src/ActionsBanner.stories.js
+++ packages/nds-action-banner/src/ActionsBanner.stories.js
@@ -1,10 +1,44 @@
 import React from "react";
 
 import { storiesOf } from "@storybook/react";
-import { withKnobs } from "@storybook/addon-knobs";
+import { withKnobs, text, select, boolean } from "@storybook/addon-knobs";
 
 import ActionBanner from "./ActionBanner";
 
 storiesOf("Action banner", module)
 	.addDecorator(withKnobs)
-	.add("Primary", () => <ActionBanner />);
+	.add("Default", () => {
+		return <ActionBanner title="Default action banner" closeable="true">
+			This is <a href="#">some content with a link</a>
+		</ActionBanner>;
+	})
+	.add("Subtle", () => {
+		return <ActionBanner modifier="subtle" title="Subtle action banner" closeable="true">
+			This is <a href="#">some content with a link</a>
+		</ActionBanner>;
+	})
+	.add("Customisable", () => {
+
+		const ctaModifiers = {
+			Default: "",
+			Inverse: "inverse",
+			Secondary: "secondary",
+			CTA: "cta"
+		};
+		const ctaType = select("CTA type", ctaModifiers, "inverse", "CTA");
+		const ctaText = text("CTA text", "Do the thing", "CTA");
+		const cta = <a href="#" className={`btn btn--${ctaType}`}>{ctaText}</a>;
+
+		const bannerModifiers = {
+			Default: "",
+			Subtle: "subtle"
+		};
+
+		return <ActionBanner
+			modifier={select("Class modifier", bannerModifiers, "", "Banner")}
+			title={text("Title", "Customisable action banner", "Banner")}
+			closeable={boolean("Closeable", true, "Banner")}
+			cta={cta}>
+			{text("Content", "This is some content for the banner message", "Banner")}
+		</ActionBanner>;
+	});
