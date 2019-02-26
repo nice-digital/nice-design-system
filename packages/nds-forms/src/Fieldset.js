c6b66d662a1cfa49f22af2cc8c6863f13d44ca92
Jose Hilario Almeida <josealmeida@gmail.com>
2019-02-26 15:36:16 +0000

    Add class name to fieldset props

--- packages/nds-forms/src/Fieldset.js
+++ packages/nds-forms/src/Fieldset.js
@@ -4,16 +4,17 @@ import "./../scss/fieldset.scss";
 
 type FieldsetProps = {
 	children: React.Node,
+	className: string,
 	legend: string,
 };
 
 export default class Fieldset extends Component<FieldsetProps> {
 
 	render() {
-		const {legend, children} = this.props;
+		const {legend, children, className} = this.props;
 
 		return (
-			<fieldset className="fieldset">
+			<fieldset className={`fieldset ${className}`} >
 				<legend className="fieldset__legend">
 					{legend}
 				</legend>
