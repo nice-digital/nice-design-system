666a82959622d2de08558b785dd53b076f2efe2d
Ian Routledge <ian.routledge@nice.org.uk>
2019-04-18 21:09:17 +0100

    Improve storybook paths to ignore node_modules

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,7 @@
 import { configure } from "@storybook/react";
 
 // automatically import all files ending in *.stories.js
-const req = require.context("../packages", true, /__stories__\/.*.stories.js$/);
+const req = require.context("../packages", true, /^((?![\\/]node_modules|vendor[\\/]).)*__stories__\/.*\.stories\.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
