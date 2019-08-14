a24f6237f50048e8add1f88bdc0e2fe62e95a99d
Warren Keith <warrenkeith@gmail.com>
2019-08-15 00:01:47 +0100

    Tidy up storybook & add icons package

--- packages/storybook/stories/nds-radio.stories.js
+++ packages/storybook/stories/nds-radio.stories.js
@@ -4,6 +4,6 @@ import { storiesOf } from "@storybook/react";
 
 import { Radio } from "@nice-digital/nds-radio";
 
-const RadioDefault = () => <Radio label="Welcome to nice here is some text"/>;
+const RadioDefault = () => <Radio label="Welcome to nice here is some text" />;
 
 storiesOf("Radio", module).add("default", RadioDefault);
