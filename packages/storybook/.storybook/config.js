12c0f317740c60400f2712cb47235c43f2790263
Ian Routledge <ian.routledge@nice.org.uk>
2019-08-22 12:26:17 +0100

    Add knobs to global storybook config

--- packages/storybook/.storybook/config.js
+++ packages/storybook/.storybook/config.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { configure, addDecorator } from "@storybook/react";
+import { withKnobs } from "@storybook/addon-knobs";
+
 import "./stories.scss";
+
+// Knobs everywhere!
+addDecorator(withKnobs);
 
 // Wrap all stories in a container
 const wrapperDecorator = storyFn => (
