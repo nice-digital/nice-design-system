ca9c55defbc34a1fadd094837f19624ac27819db
Warren Keith <warrenkeith@gmail.com>
2019-08-28 13:22:04 +0100

    Radio stories

--- packages/storybook/stories/nds-radio.stories.js
+++ packages/storybook/stories/nds-radio.stories.js
@@ -3,13 +3,56 @@
 import React from "react";
 
 import { storiesOf } from "@storybook/react";
-
+import { text, boolean } from "@storybook/addon-knobs";
 import { Radio } from "@nice-digital/nds-radio";
 
-const RadioDefault = () => (
+const RadioDefault = () => <Radio label="Yes" />;
+
+const RadioError = () => (
+	<Radio value="error" label="Error!" error={true} group="my-group" />
+);
+
+const RadioDisabled = () => (
+	<Radio value="disabled" label="Disabled!" disabled={true} group="my-group" />
+);
+
+const RadioInline = () => (
 	<div>
-		<Radio label="Yes" />
+		<Radio value="yes" label="Yes, please." group="my-group" inline={true} />
+		<Radio value="no" label="No, thank you." group="my-group" inline={true} />
 	</div>
 );
 
-storiesOf("Radio", module).add("default", RadioDefault);
+const RadioCustomise = () => (
+	<div>
+		<Radio
+			value="yes"
+			label="Yes, please"
+			group="my-group"
+			inline={boolean("Inline")}
+		/>
+		<Radio
+			value={text("Value", "no")}
+			label={text("Label", "No, thank you.")}
+			disabled={boolean("Disabled")}
+			error={boolean("Error")}
+			group="my-group"
+			inline={boolean("Inline")}
+		/>
+	</div>
+);
+
+storiesOf("Radio", module)
+	.add("Default", RadioDefault)
+	.add("Error", RadioError)
+	.add("Disabled", RadioDisabled)
+	.add("Inline", RadioInline)
+	.add("Try it out", RadioCustomise);
+
+// children?: React.ReactNode;
+// 	disabled?: boolean;
+// 	error?: boolean;
+// 	group: string;
+// 	inline?: boolean;
+// 	label: string;
+// 	value: string;
