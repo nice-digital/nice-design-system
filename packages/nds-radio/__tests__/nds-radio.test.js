be3535702a2bba3d8be92344f468b1372092b9ca
Warren <warren.keith@nice.org.uk>
2019-08-30 10:50:24 +0100

    Radio test format tidy up

--- packages/nds-radio/__tests__/nds-radio.test.js
+++ packages/nds-radio/__tests__/nds-radio.test.js
@@ -18,42 +18,42 @@ describe("Radio", () => {
 		const wrapper = shallow(<Radio {...props} />);
 		expect(wrapper).toHaveLength(1);
 	});
-});
 
-it("should match snapshot with some default attributes", () => {
-	const wrapper = shallow(<Radio {...props} />);
-	expect(toJson(wrapper)).toMatchSnapshot();
-});
+	it("should match snapshot with some default attributes", () => {
+        const wrapper = shallow(<Radio {...props} />);
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
 
-it("should not render a radio control if it doesn't have a value", () => {
-	const localProps = Object.assign({}, props);
-	localProps.value = "";
-	const wrapper = shallow(<Radio {...localProps} />);
-	expect(wrapper.isEmptyRender()).toBe(true);
-});
+    it("should not render a radio control if it doesn't have a value", () => {
+        const localProps = Object.assign({}, props);
+        localProps.value = "";
+        const wrapper = shallow(<Radio {...localProps} />);
+        expect(wrapper.isEmptyRender()).toBe(true);
+    });
 
-it("should prevent actions if the button is disabled", () => {
-	const localProps = Object.assign({}, props);
-	localProps.disabled = true;
-	const wrapper = shallow(<Radio {...localProps} />);
-	const input = wrapper.find("input[disabled]");
-	expect(input.length).toEqual(1);
-});
+    it("should prevent actions if the button is disabled", () => {
+        const localProps = Object.assign({}, props);
+        localProps.disabled = true;
+        const wrapper = shallow(<Radio {...localProps} />);
+        const input = wrapper.find("input[disabled]");
+        expect(input.length).toEqual(1);
+    });
 
-it("should add an error class to the parent if the error prop is true", () => {
-	const localProps = Object.assign({}, props);
-	localProps.error = true;
-	const wrapper = shallow(<Radio {...localProps} />);
-	const parent = wrapper.find("input[disabled]");
-	expect(parent.length).toEqual(1);
-});
+    it("should add an error class to the parent if the error prop is true", () => {
+        const localProps = Object.assign({}, props);
+        localProps.error = true;
+        const wrapper = shallow(<Radio {...localProps} />);
+        const parent = wrapper.find("input[disabled]");
+        expect(parent.length).toEqual(1);
+    });
 
-it("should pass through any additional attributes supplied", () => {
-	const localProps = Object.assign({}, props);
-	localProps.defaultChecked = true;
-	localProps["data-something"] = "test";
-	const wrapper = mount(<Radio {...localProps} />);
-	const checked = wrapper.find("input[defaultChecked]");
-	expect(checked.length).toEqual(1);
-	expect(checked.props()["data-something"]).toEqual("test");
+    it("should pass through any additional attributes supplied", () => {
+        const localProps = Object.assign({}, props);
+        localProps.defaultChecked = true;
+        localProps["data-something"] = "test";
+        const wrapper = mount(<Radio {...localProps} />);
+        const checked = wrapper.find("input[defaultChecked]");
+        expect(checked.length).toEqual(1);
+        expect(checked.props()["data-something"]).toEqual("test");
+    });
 });
