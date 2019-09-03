a8305f1a36b142b4f7d21e4608ca8d3e3e7876df
Warren Keith <warrenkeith@gmail.com>
2019-09-03 12:15:42 +0100

    Add routing to the dev-app

---
+++ packages/dev-app/src/views/FormGroupView.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const FormGroupView = () => {
+	return (
+		<>
+			<h1>Form Group View</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate,
+				vitae molestiae nisi aut quasi ipsam sed eveniet quod adipisci
+				architecto repellat tenetur beatae asperiores consectetur laborum saepe
+				iste dolore provident.
+			</p>
+		</>
+	);
+};
