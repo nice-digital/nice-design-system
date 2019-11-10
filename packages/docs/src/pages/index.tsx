3943df03cf09dba732f2d954af7eae0c42cb7ac9
Warren Keith <warrenkeith@gmail.com>
2019-11-10 22:53:34 +0000

    Add typescript to docs

--- packages/docs/src/pages/index.tsx
+++ packages/docs/src/pages/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { Hero } from "@nice-digital/design-system";
 
 const IndexPage = () => (
 	<div>
+		<Hero title="Design System">This is the hero</Hero>
 		<p>It might work one day...</p>
 	</div>
 );
