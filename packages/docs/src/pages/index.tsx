9f8101e7b83fe86d6305258649e68004b21065ea
Warren Keith <warrenkeith@gmail.com>
2019-11-11 09:41:45 +0000

    Unify Babel and Node-Sass versions across leaves

--- packages/docs/src/pages/index.tsx
+++ packages/docs/src/pages/index.tsx
@@ -1,9 +1,21 @@
 import React from "react";
-import { Hero } from "@nice-digital/design-system";
+import { Hero } from "@nice-digital/nds-hero";
 
 const IndexPage = () => (
 	<div>
-		<Hero title="Design System">This is the hero</Hero>
+		<Hero title="NICE Design System" intro="If you build it, they will come">
+			<ul className="list--unstyled">
+				<li>
+					<a href="#">About the system</a>
+				</li>
+				<li>
+					<a href="#">How to use</a>
+				</li>
+				<li>
+					<a href="#">Feedback &amp; contributing</a>
+				</li>
+			</ul>
+		</Hero>
 		<p>It might work one day...</p>
 	</div>
 );
