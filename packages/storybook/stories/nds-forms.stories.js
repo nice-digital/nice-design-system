1d18d83da309061f3d383eb4bc93bdae5570c98d
Warren Keith <warrenkeith@gmail.com>
2020-05-05 13:01:19 +0100

    Add nds-forms stories

--- packages/storybook/stories/nds-forms.stories.js
+++ packages/storybook/stories/nds-forms.stories.js
@@ -5,13 +5,27 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { text, boolean, select } from "@storybook/addon-knobs";
 import { Input, Textarea } from "@nice-digital/nds-forms";
+import { FormGroup } from "@nice-digital/nds-form-group";
 
-const CheckboxDefault = () => (
-	<div>
-		<Input name="firstname" label="First name" unique="firstname"/>
-		<Textarea label="Description" name="description"/>
-	</div>
+const InputComponent = () => (
+	<Input
+		error={boolean("Error state")}
+		errorMessage={text("Error message", "This field is required")}
+		label={text("label", "First name")}
+		name="firstname"
+		unique="firstname"
+	/>
+);
+
+const TextareaComponent = () => (
+	<Textarea
+		error={boolean("Error state")}
+		errorMessage={text("Error message", "This field is required")}
+		label={text("label", "Description")}
+		unique="description"
+	/>
 );
 
 storiesOf("Forms", module)
-	.add("Default", CheckboxDefault);
+	.add("Input - try it out", InputComponent)
+	.add("Textarea - try it out", TextareaComponent);
