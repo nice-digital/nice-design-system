242c2c7206a9434cdef634ab6d1b857a95b4abf1
Warren Keith <w@rren.dev>
2020-05-27 11:39:27 +0100

    Typo

--- components/nds-simple-pagination/nds-simple-pagination.d.ts
+++ components/nds-simple-pagination/nds-simple-pagination.d.ts
@@ -15,17 +15,3 @@ declare module "@nice-digital/nds-simple-pagination" {
 
 	export const SimplePagination: React.FC<SimplePaginationProps>;
 }
-
-##### link.destination
-
-- Type: `string`
-- Default: `null`
-
-A string to represent the destination if the card heading is a link.
-
-	##### link.elementType
-
-- Type: `React.ElementType`
-- Default: `a`
-
-The tag that you would like to use for the heading link. By default it's an HTMLAnchorElement and will result in something like `<a href="/destination">Card heading text</a>` but you can pass a custom router link tag such as `Link` to result in `<Link to="/destination">Card heading text</Link>`.
