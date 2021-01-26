437500dc8fac1761cfd570e3a04913fc9c2e6fb1
w@rren <w@rren.dev>
2021-01-26 12:28:25 +0000

    Babel presets for gatsby to run docs as CWD

--- packages/docs/gatsby-config.js
+++ packages/docs/gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
-	// flags: {
-	// 	PRESERVE_WEBPACK_CACHE: true
-	// },
+	flags: {
+		PRESERVE_WEBPACK_CACHE: true
+	},
 	siteMetadata: {
 		title: "NICE Design System",
 		description:
