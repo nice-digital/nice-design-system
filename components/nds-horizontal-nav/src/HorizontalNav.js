fc613e08ee0a93ed7d4b500e78d1f348e3bce31f
w@rren <w@rren.dev>
2021-02-17 12:06:28 +0000

    NDS-118 Horizontal Nav basic component

--- components/nds-horizontal-nav/src/HorizontalNav.js
+++ components/nds-horizontal-nav/src/HorizontalNav.js
@@ -1,9 +1,48 @@
 import React from "react";
-// import PropTypes from "prop-types";
+import PropTypes from "prop-types";
 // import classnames from "classnames";
 
 import "./../scss/horizontal-nav.scss";
 
-export const HorizontalNav = props => {
-	return <p className="horizontal-nav">{JSON.stringify(props)}</p>;
+export const HorizontalNav = ({ children, ...rest }) => {
+	return (
+		<nav className="horizontal-nav" {...rest}>
+			<ul className="horizontal-nav__list">{children}</ul>
+		</nav>
+	);
+};
+
+export const HorizontalNavLink = ({
+	title,
+	isCurrent = false,
+	destination,
+	elementType: ElementType = "a",
+	children
+}) => {
+	const props = {
+		"aria-current": isCurrent,
+		className: "horizontal-nav__link"
+	};
+	if (ElementType === "a") {
+		props.href = destination;
+	} else {
+		props.to = destination;
+	}
+	return (
+		<li className="horizontal-nav__item">
+			<ElementType {...props}>{children || title}</ElementType>
+		</li>
+	);
+};
+
+HorizontalNav.propTypes = {
+	children: PropTypes.node
+};
+
+HorizontalNavLink.propTypes = {
+	children: PropTypes.node,
+	title: PropTypes.string,
+	isCurrent: PropTypes.bool,
+	destination: PropTypes.string,
+	elementType: PropTypes.element
 };
--- packages/storybook/stories/nds-horizontal-nav.stories.js
+++ packages/storybook/stories/nds-horizontal-nav.stories.js
@@ -1,9 +1,51 @@
 /* eslint react/prop-types: 0 */
 import React from "react";
-import { HorizontalNav } from "@nice-digital/nds-horizontal-nav";
+import {
+	HorizontalNav,
+	HorizontalNavLink
+} from "@nice-digital/nds-horizontal-nav";
 
 import { storiesOf } from "@storybook/react";
 
-const Default = () => <HorizontalNav monkey="true" />;
+const links = [
+	{
+		title: "Guidance",
+		destination: "#",
+		isCurrent: false,
+		elementType: "a"
+	},
+	{
+		title: "Tools and resources",
+		destination: "#",
+		isCurrent: true,
+		elementType: "a"
+	},
+	{
+		title: "Information for the public",
+		destination: "#",
+		isCurrent: false,
+		elementType: "a"
+	},
+	{
+		title: "Evidence",
+		destination: "#",
+		isCurrent: false,
+		elementType: "a"
+	},
+	{
+		title: "History",
+		destination: "#",
+		isCurrent: false,
+		elementType: "a"
+	}
+];
+
+const Default = () => (
+	<HorizontalNav>
+		{links.map(link => (
+			<HorizontalNavLink key={link.title} {...link} />
+		))}
+	</HorizontalNav>
+);
 
 storiesOf("Components/Horizontal Nav", module).add("Default", Default);
