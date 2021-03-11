d53dabdc835b1d9cfefe7eb669e4e02bd8c1c769
w@rren <w@rren.dev>
2021-03-11 17:24:42 +0000

    NDS-146 breadcrumbs accept undefined (#168)
    * Breadcrumbs accept undefined as children
    
    * Grid accepts undefined as child

--- packages/dev-app/src/views/BreadcrumbsView.tsx
+++ packages/dev-app/src/views/BreadcrumbsView.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Breadcrumbs, Breadcrumb } from "@nice-digital/nds-breadcrumbs";
 
+const monkey = null;
+
 export const BreadcrumbsView = () => {
 	return (
 		<Breadcrumbs>
@@ -9,6 +11,7 @@ export const BreadcrumbsView = () => {
 			<Breadcrumb>How</Breadcrumb>
 			<Breadcrumb>Are</Breadcrumb>
 			<Breadcrumb>You</Breadcrumb>
+			{monkey && <Breadcrumb>Monkey</Breadcrumb>}
 		</Breadcrumbs>
 	);
 };
