94ffdca40754156ff80013ea1fd9966368ad1416
Warren <w@rren.dev>
2021-07-08 13:12:26 +0100

    dart sass madness

--- components/icons/examples/simple-express/index.js
+++ components/icons/examples/simple-express/index.js
@@ -1,29 +1,34 @@
 const express = require("express"),
 	path = require("path"),
-	sass = require("node-sass"),
+	sass = require("sass"),
 	fs = require("fs");
 
 // Create CSS file.
 // In production this would be through a separate build process
-sass.render({
-	file: path.join(__dirname, "./app.scss"),
-	includePaths: [
-		path.join(__dirname, "./node_modules/@nice-digital/icons/scss")
-	]
-}, function(err, result) {
-	if (err) {
-		console.log(err.status);
-		console.log(err.column);
-		console.log(err.message);
-		console.log(err.line);
-	} else {
-		fs.writeFile(path.join(__dirname, "./app.css"), result.css, function(err){
-			if(!err){
-				console.log("SASS compiled");
-			}
-		});
+sass.render(
+	{
+		file: path.join(__dirname, "./app.scss"),
+		includePaths: [
+			path.join(__dirname, "./node_modules/@nice-digital/icons/scss")
+		]
+	},
+	function(err, result) {
+		if (err) {
+			console.log(err.status);
+			console.log(err.column);
+			console.log(err.message);
+			console.log(err.line);
+		} else {
+			fs.writeFile(path.join(__dirname, "./app.css"), result.css, function(
+				err
+			) {
+				if (!err) {
+					console.log("SASS compiled");
+				}
+			});
+		}
 	}
-});
+);
 
 // Create express app, using the webfont directly from the npm package
 const app = express();
@@ -33,7 +38,12 @@ app.get("/", function(req, res) {
 app.get("/app.css", function(req, res) {
 	res.sendFile(path.join(__dirname, "./app.css"));
 });
-app.use("/fonts", express.static(path.join(__dirname, "./node_modules/@nice-digital/icons/dist")));
+app.use(
+	"/fonts",
+	express.static(
+		path.join(__dirname, "./node_modules/@nice-digital/icons/dist")
+	)
+);
 app.listen(3000);
 
 console.log("Running on port 3000");
