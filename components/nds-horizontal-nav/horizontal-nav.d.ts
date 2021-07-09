5b6855f0a642745c33073628ad1061baf602dee5
Warren <w@rren.dev>
2021-07-09 22:42:10 +0100

    NDS-172 Github issue #213

--- components/nds-horizontal-nav/horizontal-nav.d.ts
+++ components/nds-horizontal-nav/horizontal-nav.d.ts
@@ -7,11 +7,12 @@ declare module "@nice-digital/nds-horizontal-nav" {
 
 	export interface HorizontalNavLinkProps {
 		children?: React.ReactNode;
-		title?: string;
-		isCurrent?: boolean;
+		className?: string;
 		destination: string;
 		elementType?: React.ReactHTMLElement<any> | React.ReactNode;
-		className?: string;
+		isCurrent?: boolean;
+		method?: string;
+		title?: string;
 	}
 
 	export const HorizontalNav: React.FC<HorizontalNavProps>;
