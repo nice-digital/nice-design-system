a8eaf90d427e6f7c0190d14ce64a71814459416e
Warren <w@rren.dev>
2021-09-07 17:21:59 +0100

    NDS-000 Move utils to central export

--- components/nds-filters/src/FilterGroup.js
+++ components/nds-filters/src/FilterGroup.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
-import { slugify } from "@nice-digital/nds-core/es/utils";
+import { slugify } from "@nice-digital/nds-core";
 
 import "./../scss/filter-panel.scss";
 
