4f628bc3457375f2906ec63eb29f53b0681a07f3
Chris Barker <chris.barker@nice.org.uk>
2022-04-01 11:20:54 +0100

    BNF-1277 Add alert colours to semantic styles

--- packages/storybook/storybook-components/Swatch.jsx
+++ packages/storybook/storybook-components/Swatch.jsx
@@ -1,6 +1,10 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-// eslint-disable-next-line react/prop-types
 export const Swatch = ({ colour }) => (
 	<span className="storybook-swatch" style={{ background: colour }}></span>
 );
+
+Swatch.propTypes = {
+	colour: PropTypes.string.isRequired
+};
