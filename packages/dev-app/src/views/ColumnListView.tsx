a71ad3663f8ffc04c6542407b8c03fc9eed0b8be
Ian Routledge <ian.routledge@nice.org.uk>
2022-07-27 09:14:36 +0100

    NDS-257 Tidy up column list spacing

--- packages/dev-app/src/views/ColumnListView.tsx
+++ packages/dev-app/src/views/ColumnListView.tsx
@@ -15,9 +15,18 @@ export const ColumnListView = () => {
 			</ColumnList>
 			<h2>Variant (plain, 2 columns)</h2>
 			<ColumnList plain columns={2}>
-				<li>Item one</li>
-				<li>Item two</li>
-				<li>Item three</li>
+				<li>
+					<a href="#">
+						Item one really really really really really really really really
+						really really really really really really long so it wraps
+					</a>
+				</li>
+				<li>
+					<a href="#">Item two</a>
+				</li>
+				<li>
+					<a href="#">Item three</a>
+				</li>
 				<li>Item four</li>
 				<li>Item five</li>
 				<li>Item six</li>
