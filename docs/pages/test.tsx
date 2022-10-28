6dc576f41d28ae80b2c1ba02c39571aa4d2433e8
Chris Barker <chris.barker@nice.org.uk>
2022-10-28 16:15:34 +0100

    NDS-337 WIP Next.js integration

---
+++ docs/pages/test.tsx
@@ -0,0 +1,9 @@
+import { ActionBanner } from "@nice-digital/nds-action-banner";
+
+export default function Test() {
+	return (
+		<ActionBanner title="A title">
+			Some body text and some more text and <a href="#">a link</a>
+		</ActionBanner>
+	);
+}
