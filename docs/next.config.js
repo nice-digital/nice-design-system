20ae6c656cc81e6becaf936b968a359abfe245a1
Chris Barker <chris.barker@nice.org.uk>
2022-10-31 17:06:49 +0000

    NDS-337 Glob NDS modules for transpilation

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,16 +1,17 @@
 /** @type {import('next').NextConfig} */
+
+const glob = require("glob");
+
 const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true
 };
 
-// All NDS components need to be manually listed here to be transpiled
-// If we don't add them here, Next will kick off about the global CSS
-const allNDSComponents = [
-	"@nice-digital/nds-action-banner",
-	"@nice-digital/nds-button"
-];
+// All NDS components need to be transpiled
+// If we don't do this, Next will kick off about the global CSS
+const allNDSComponents = glob.sync("@nice-digital/*", {
+	cwd: "../node_modules"
+});
 
 const withTM = require("next-transpile-modules")(allNDSComponents);
-
 module.exports = withTM(nextConfig);
