d0d6d059d705016b9aeaff31fdc370e4bb9a0d2f
Chris Barker <chris.barker@nice.org.uk>
2022-11-01 11:19:23 +0000

    NDS-337 Add MDX support

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,11 +1,29 @@
 /** @type {import('next').NextConfig} */
 
 const glob = require("glob");
+const { default: next } = require("next");
+const {
+	getModuleBuildInfo
+} = require("next/dist/build/webpack/loaders/get-module-build-info");
 
 const nextConfig = {
 	reactStrictMode: true,
-	swcMinify: true
+	swcMinify: true,
+	pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"]
 };
+
+const withMDX = require("@next/mdx")({
+	extension: /\.mdx?$/,
+	options: {
+		// If you use remark-gfm, you'll need to use next.config.mjs
+		// as the package is ESM only
+		// https://github.com/remarkjs/remark-gfm#install
+		remarkPlugins: [],
+		rehypePlugins: []
+		// If you use `MDXProvider`, uncomment the following line.
+		// providerImportSource: "@mdx-js/react",
+	}
+});
 
 // All NDS components need to be transpiled
 // If we don't do this, Next will kick off about the global CSS
@@ -14,4 +32,5 @@ const allNDSComponents = glob.sync("@nice-digital/*", {
 });
 
 const withTM = require("next-transpile-modules")(allNDSComponents);
-module.exports = withTM(nextConfig);
+
+module.exports = withTM(withMDX(nextConfig));
