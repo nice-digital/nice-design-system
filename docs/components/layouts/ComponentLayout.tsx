d90ea8810cfcadf13c59b5321df8d320be93c4dc
Chris Barker <chris.barker@nice.org.uk>
2022-11-24 15:07:07 +0000

    NDS-337 Style component docs page

--- docs/components/layouts/ComponentLayout.tsx
+++ docs/components/layouts/ComponentLayout.tsx
@@ -1,33 +1,75 @@
 import { ReactNode } from "react";
+import Head from "next/head";
+import Link from "next/link";
 import { StackedNav, StackedNavLink } from "@nice-digital/nds-stacked-nav";
 import { Grid, GridItem } from "@nice-digital/nds-grid";
-import Link from "next/link";
+
+import { type PageMeta } from "types/meta";
+import styles from "./ComponentLayout.module.scss";
 
 export interface ComponentLayoutProps {
 	children: ReactNode;
+	meta: PageMeta;
 }
 
-export function ComponentLayout({ children }: ComponentLayoutProps) {
+export function ComponentLayout({ children, meta }: ComponentLayoutProps) {
+	const shouldShowExternalLinks = meta.gitHubUrl || meta.npmUrl;
+
 	return (
-		<Grid>
-			<GridItem cols={12} sm={{ cols: 2 }}>
-				<StackedNav label="Components menu" elementType="h2">
-					<StackedNavLink elementType={Link} method="href" destination="/">
-						NDS Docs Home
-					</StackedNavLink>
-					<StackedNavLink
-						elementType={Link}
-						method="href"
-						destination="/components/action-banner"
-					>
-						Action Banner
-					</StackedNavLink>
-				</StackedNav>
-			</GridItem>
-			<GridItem cols={12} sm={{ cols: 10 }}>
-				TODO: Add breadcrumbs, on page nav, NPM/GitHub links
-				{children}
-			</GridItem>
-		</Grid>
+		<>
+			<Head>
+				<title>{`${meta.title} | NICE Design System`}</title>
+			</Head>
+			TODO: Add breadcrumbs here
+			<Grid>
+				<GridItem cols={12} md={{ cols: 2 }}>
+					<StackedNav label="Components menu" elementType="h2">
+						<StackedNavLink elementType={Link} method="href" destination="/">
+							NDS Docs Home
+						</StackedNavLink>
+						<StackedNavLink
+							elementType={Link}
+							method="href"
+							destination="/components/action-banner"
+						>
+							Action Banner
+						</StackedNavLink>
+					</StackedNav>
+				</GridItem>
+				<GridItem cols={12} md={{ cols: 8 }}>
+					{children}
+				</GridItem>
+				<GridItem cols={12} md={{ cols: 2 }}>
+					<p>TODO: Add on-page nav</p>
+
+					{shouldShowExternalLinks && (
+						<ul className={styles.externalLinkList}>
+							{meta.gitHubUrl && (
+								<li>
+									<a
+										className={`btn btn--secondary ${styles.externalSiteButton}`}
+										href={meta.gitHubUrl}
+									>
+										<img src="/github.svg" alt="" />
+										View on GitHub
+									</a>
+								</li>
+							)}
+							{meta.npmUrl && (
+								<li>
+									<a
+										className={`btn btn--secondary ${styles.externalSiteButton}`}
+										href={meta.npmUrl}
+									>
+										<img src="/npm.svg" alt="" />
+										View on NPM
+									</a>
+								</li>
+							)}
+						</ul>
+					)}
+				</GridItem>
+			</Grid>
+		</>
 	);
 }
---
+++ docs/types/meta.ts
@@ -0,0 +1,5 @@
+export interface PageMeta {
+	title: string;
+	npmUrl: string;
+	gitHubUrl: string;
+}
