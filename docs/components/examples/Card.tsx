4532c76efbc5a3d3017414050c0d60cc319001c0
Chris Barker <chris.barker@nice.org.uk>
2023-01-24 10:06:46 +0000

    NDS-337 Add Example wrapper component

--- docs/components/examples/Card.tsx
+++ docs/components/examples/Card.tsx
@@ -1,29 +1,34 @@
+import { Example } from "./Example";
 import { Card } from "@nice-digital/nds-card";
 import { Grid, GridItem } from "@nice-digital/nds-grid";
 
 export const ExampleCardList = () => (
-	<ul className="list--unstyled">
-		<li>
-			<Card headingText="Card title 1" link={{ destination: "/test" }}>
-				Card text 1
-			</Card>
-		</li>
-		<li>
-			<Card headingText="Card title 2">Card text 2</Card>
-		</li>
-	</ul>
+	<Example>
+		<ul className="list--unstyled">
+			<li>
+				<Card headingText="Card title 1" link={{ destination: "/test" }}>
+					Card text 1
+				</Card>
+			</li>
+			<li>
+				<Card headingText="Card title 2">Card text 2</Card>
+			</li>
+		</ul>
+	</Example>
 );
 
 export const ExampleCardGrid = () => (
-	<Grid>
-		<GridItem cols={12} sm={{ cols: 4 }}>
-			<Card headingText="Card title 2">Card text 2</Card>
-		</GridItem>
-		<GridItem cols={12} sm={{ cols: 4 }}>
-			<Card headingText="Card title 2">Card text 2</Card>
-		</GridItem>
-		<GridItem cols={12} sm={{ cols: 4 }}>
-			<Card headingText="Card title 2">Card text 2</Card>
-		</GridItem>
-	</Grid>
+	<Example>
+		<Grid>
+			<GridItem cols={12} sm={{ cols: 4 }}>
+				<Card headingText="Card title 2">Card text 2</Card>
+			</GridItem>
+			<GridItem cols={12} sm={{ cols: 4 }}>
+				<Card headingText="Card title 2">Card text 2</Card>
+			</GridItem>
+			<GridItem cols={12} sm={{ cols: 4 }}>
+				<Card headingText="Card title 2">Card text 2</Card>
+			</GridItem>
+		</Grid>
+	</Example>
 );
