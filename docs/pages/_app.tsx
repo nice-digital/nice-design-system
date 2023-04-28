d98dbacac91f5ca41d6e307837e8ae4a05320a42
Chris Barker <chris@christopherbarker.co.uk>
2023-04-28 14:09:46 +0100

    NDS-382 Move font families into CSS vars (#310)
    Co-authored-by: Chris Barker <chris.barker@nice.org.uk>

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -1,12 +1,29 @@
 import "../styles/globals.scss";
 import "@code-hike/mdx/dist/index.css";
 import type { AppProps } from "next/app";
+import { Inter, Lora } from "next/font/google";
 import { RootLayout } from "../components/layouts/RootLayout";
+
+const inter = Inter({
+	subsets: ["latin"]
+});
+
+const lora = Lora({
+	subsets: ["latin"]
+});
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<RootLayout>
-			<Component {...pageProps} />
-		</RootLayout>
+		<>
+			<style jsx global>{`
+				html {
+					--sans-font-family: ${inter.style.fontFamily};
+					--serif-font-family: ${lora.style.fontFamily};
+				}
+			`}</style>
+			<RootLayout>
+				<Component {...pageProps} />
+			</RootLayout>
+		</>
 	);
 }
