9f274ec100bf11b5ade4abfdd58848ca3bb40b1f
Chris Barker <chris.barker@nice.org.uk>
2023-05-03 15:46:21 +0100

    NDS-383 Trivial change to force publish

--- components/nds-filters/src/FilterSummary.tsx
+++ components/nds-filters/src/FilterSummary.tsx
@@ -179,6 +179,7 @@ function ResultsSorting({ sorting, selectName = "s" }: ResultsSortingProps) {
 					);
 				})}
 			</select>
+
 			{showButton && <button type="submit">Apply sorting</button>}
 		</div>
 	);
