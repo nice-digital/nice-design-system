ebaf6cef9d4f14f03e98f018205c93e6f170a0c1
Chris Barker <chris.barker@nice.org.uk>
2023-05-12 15:22:55 +0100

    NDS-377 Add page header component

---
+++ docs/components/examples/PageHeader.tsx
@@ -0,0 +1,23 @@
+import { PageHeader } from "@nice-digital/nds-page-header";
+import { Button } from "@nice-digital/nds-button";
+
+export const DefaultHeader = () => <PageHeader heading="Welcome to the page" />;
+
+export const PreHeader = () => (
+	<PageHeader heading="Welcome to the page" preheading="Here's a pre-heading" />
+);
+
+export const LeadHeader = () => (
+	<PageHeader heading="Welcome to the page" lead="This is the lead" />
+);
+
+export const MetadataHeader = () => (
+	<PageHeader heading="Welcome to the page" metadata={["Item 1", "Item 2"]} />
+);
+
+export const CTAHeader = () => (
+	<PageHeader
+		heading="Welcome to the page"
+		cta={<Button>Do something</Button>}
+	/>
+);
