fb25ba5129e81cd4a7b680f6f07401af4d47c697
Chris Barker <chris.barker@nice.org.uk>
2023-05-24 13:39:36 +0100

    NDS-377 Add design library page

---
+++ docs/pages/design-library.tsx
@@ -0,0 +1,71 @@
+import Head from "next/head";
+import { Breadcrumb, Breadcrumbs } from "@nice-digital/nds-breadcrumbs";
+import { Grid, GridItem } from "@nice-digital/nds-grid";
+import { Card } from "@nice-digital/nds-card";
+import { PageHeader } from "@nice-digital/nds-page-header";
+
+import { type PageData } from "types/PageData";
+import { components } from "../data/components";
+import { foundations } from "../data/foundations";
+
+export default function Test() {
+	return (
+		<>
+			<Head>
+				<title>Design Library | NICE Design System</title>
+			</Head>
+			<Breadcrumbs>
+				<Breadcrumb to="/">NICE Design System</Breadcrumb>
+				<Breadcrumb>Design Library</Breadcrumb>
+			</Breadcrumbs>
+
+			<PageHeader
+				heading="Design Library"
+				lead="A reference for all the components, foundations and patterns in the NICE design system"
+			/>
+
+			<h2 id="foundations">Foundations</h2>
+			<p className="lead">
+				Our foundations are the design basics that we build our services on.
+			</p>
+
+			<Grid>
+				{foundations.map(
+					({ slug, title, description }: PageData, index: number) => (
+						<GridItem key={index} sm={4}>
+							<Card
+								headingText={title}
+								link={{ destination: `/foundations/${slug}` }}
+							>
+								{description && <p>{description}</p>}
+							</Card>
+						</GridItem>
+					)
+				)}
+			</Grid>
+
+			<hr />
+
+			<h2 id="components">Components</h2>
+			<p className="lead">
+				Components are individual design building blocks. They can be combined
+				to create larger patterns and pages.
+			</p>
+
+			<Grid>
+				{components.map(
+					({ slug, title, description }: PageData, index: number) => (
+						<GridItem key={index} sm={4}>
+							<Card
+								headingText={title}
+								link={{ destination: `/components/${slug}` }}
+							>
+								{description && <p>{description}</p>}
+							</Card>
+						</GridItem>
+					)
+				)}
+			</Grid>
+		</>
+	);
+}
--- docs/pages/index.tsx
+++ docs/pages/index.tsx
@@ -1,13 +1,8 @@
 import Head from "next/head";
-import Link from "next/link";
 import { Hero } from "@nice-digital/nds-hero";
 import { Breadcrumb, Breadcrumbs } from "@nice-digital/nds-breadcrumbs";
 import { Grid, GridItem } from "@nice-digital/nds-grid";
 import { Card } from "@nice-digital/nds-card";
-
-import { type PageData } from "types/PageData";
-import { components } from "../data/components";
-import { foundations } from "../data/foundations";
 
 export default function Home() {
 	return (
@@ -31,79 +26,40 @@ export default function Home() {
 				}
 			/>
 
-			<h2>Foundations</h2>
+			<h2>Design Library</h2>
 			<p className="lead">
-				Our foundations are the design basics that we build our services on.
+				A reference for all of the design elements used to build digital
+				services at NICE.
 			</p>
 
 			<Grid>
-				{foundations.map(
-					({ slug, title, description }: PageData, index: number) => (
-						<GridItem key={index} sm={4}>
-							<Card
-								headingText={title}
-								link={{ destination: `/foundations/${slug}` }}
-							>
-								{description && <p>{description}</p>}
-							</Card>
-						</GridItem>
-					)
-				)}
-			</Grid>
-
-			<hr />
-
-			<h2>Components</h2>
-			<p className="lead">
-				Components are individual design building blocks. They can be combined
-				to create larger patterns and pages.
-			</p>
-
-			<Grid>
-				{components.map(
-					({ slug, title, description }: PageData, index: number) => (
-						<GridItem key={index} sm={4}>
-							<Card
-								headingText={title}
-								link={{ destination: `/components/${slug}` }}
-							>
-								{description && <p>{description}</p>}
-							</Card>
-						</GridItem>
-					)
-				)}
-			</Grid>
-
-			<hr />
-
-			<h2 id="development">Development</h2>
-			<Grid>
 				<GridItem sm={4}>
-					<h3>Github</h3>
-					<p>
-						<Link href="https://github.com/nice-digital/nice-design-system">
-							Check out the NDS GitHub repo
-						</Link>{" "}
-						to read technical documentation and inspect the code
-					</p>
+					<Card
+						headingText="All elements"
+						link={{ destination: "/design-library" }}
+					>
+						Not sure where to start? View the whole library and find the
+						elements you need.
+					</Card>
 				</GridItem>
 				<GridItem sm={4}>
-					<h3>NPM</h3>
-					<p>
-						<Link href="https://www.npmjs.com/package/@nice-digital/design-system">
-							Install the node package using NPM
-						</Link>{" "}
-						to import styles and components into your project
-					</p>
+					<Card
+						headingText="Foundations"
+						link={{ destination: "/design-library/#foundations" }}
+					>
+						Foundations are the design basics that we build our services on,
+						including layout, typography and colour.
+					</Card>
 				</GridItem>
 				<GridItem sm={4}>
-					<h3>CDN</h3>
-					<p>
-						If you can&apos;t use NPM, you can{" "}
-						<Link href="https://cdn.nice.org.uk/niceorg/css/app.min.css">
-							import the design system styles directly via the CDN
-						</Link>
-					</p>
+					<Card
+						headingText="Components"
+						link={{ destination: "/design-library/#components" }}
+					>
+						Components are individual design building blocks such as forms,
+						filters and menus. They can be combined to build larger patterns and
+						pages.
+					</Card>
 				</GridItem>
 			</Grid>
 
