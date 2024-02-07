82a394f9017a510ce998162d635cb7476ef1a190
John Davey <john.davey@nice.org.uk>
2024-02-07 14:47:17 +0000

    Revert "NDS-412 Update action banner for new variant"
    This reverts commit 3557cac46c2d3a924fa6cfcf195fa093aab286e5.

--- components/nds-action-banner/src/ActionBanner.tsx
+++ components/nds-action-banner/src/ActionBanner.tsx
@@ -1,94 +1,66 @@
 import React, { ReactNode, useState } from "react";
-// import classnames from "classnames";
-// import RemoveIcon from "@nice-digital/icons/lib/Remove";
-import { Grid, GridItem } from "@nice-digital/nds-grid";
+import classnames from "classnames";
+import RemoveIcon from "@nice-digital/icons/lib/Remove";
 
 import "../scss/action-banner.scss";
 
 interface ActionBannerProps {
-	[prop: string]: unknown;
-	heading: React.ReactNode;
-	description: React.ReactNode;
-	variant?: "default" | "subtle" | "fullWidth" | "fullWidthSubtle";
+	title: string;
+	variant?: "default" | "subtle";
+	children: ReactNode[] | ReactNode;
 	cta?: ReactNode;
+	onClosing?: Function;
+	className?: string;
 }
 
 export const ActionBanner: React.FC<ActionBannerProps> = (
 	props
 ): JSX.Element | null => {
-	const { heading, description, variant, cta, ...rest } = props;
+	const { variant, onClosing, title, children, cta, className, ...rest } =
+		props;
 
-	const variantClasses = {
-		subtle: "action-banner--subtle",
-		fullWidthSubtle: "action-banner--full-width-subtle",
-		fullWidth: "action-banner--full-width"
+	const [isClosed, setIsClosed] = useState(false);
+
+	const closeClickHandler = () => {
+		setIsClosed(true);
+		if (typeof onClosing === "function") onClosing();
+		else throw new Error("The onClosing prop should be a function");
 	};
 
-	// Figure out variant
-	const isFullWidth =
-		variant === "fullWidth" || props.variant === "fullWidthSubtle";
+	const classes = {
+		"action-banner": true,
+		"action-banner--closeable": onClosing,
+		[`action-banner--${variant}`]: variant,
+		[`${className}`]: className
+	};
 
-	// TODO: Refactor this into some sort of conditional component
-	// e.g. https://dev.to/dailydevtips1/conditional-wrapping-in-react-46o5
-	const ActionBannerContent: React.FC = () => (
-		<Grid>
-			<GridItem md={{ cols: 12 }}>
-				<h1 className="action-banner__heading">{heading}</h1>
-
-				{description && (
-					<div className="action-banner__description">{description}</div>
-				)}
-
-				{cta && <p className="action-banner__cta">{cta}</p>}
-			</GridItem>
-		</Grid>
-	);
+	if (isClosed) return null;
 
 	return (
-		<div
-			className={`action-banner ${
-				variant ? variantClasses[variant as keyof typeof variantClasses] : ""
-			}`}
-			data-component="action-banner"
+		<section
+			className={classnames(classes)}
+			data-component={`action-banner${variant ? `--${variant}` : ""}`}
 			{...rest}
 		>
-			{isFullWidth ? (
-				<div className="container">
-					<ActionBannerContent />
+			<div className="action-banner__container">
+				<div className="action-banner__inner">
+					<div className="action-banner__text">
+						<h2 className="action-banner__title">{title}</h2>
+						{children && <div className="action-banner__intro">{children}</div>}
+					</div>
+					{cta && <div className="action-banner__actions">{cta}</div>}
+					{onClosing && (
+						<button
+							type="button"
+							className="action-banner__close"
+							onClick={closeClickHandler}
+						>
+							<RemoveIcon />
+							<span className="visually-hidden">Close {title}</span>
+						</button>
+					)}
 				</div>
-			) : (
-				<ActionBannerContent />
-			)}
-		</div>
+			</div>
+		</section>
 	);
-
-	// return (
-	// 	<section
-	// 		className={classnames(classes)}
-	// 		data-component={`action-banner${
-	// 			props.variant ? `--${props.variant}` : ""
-	// 		}`}
-	// 		{...rest}
-	// 	>
-	// 		<div className="action-banner__container">
-	// 			<div className="action-banner__inner">
-	// 				<div className="action-banner__text">
-	// 					<h2 className="action-banner__heading">{heading}</h2>
-	// 					{children && <div className="action-banner__intro">{children}</div>}
-	// 				</div>
-	// 				{cta && <div className="action-banner__actions">{cta}</div>}
-	// 				{onClosing && (
-	// 					<button
-	// 						type="button"
-	// 						className="action-banner__close"
-	// 						onClick={closeClickHandler}
-	// 					>
-	// 						<RemoveIcon />
-	// 						<span className="visually-hidden">Close {heading}</span>
-	// 					</button>
-	// 				)}
-	// 			</div>
-	// 		</div>
-	// 	</section>
-	// );
 };
