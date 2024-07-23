a66d29235bf58b112252c9f12a03760ea5bcecd8
Dale H <dale.hudson@nice.org.uk>
2024-07-23 11:11:42 +0100

    NDS-426 WIP: update docs for accordion

--- docs/components/examples/Accordion.tsx
+++ docs/components/examples/Accordion.tsx
@@ -1,4 +1,4 @@
-import { Accordion } from "@nice-digital/nds-accordion";
+import { Accordion, AccordionGroup } from "@nice-digital/nds-accordion";
 
 export const DefaultAccordion = () => (
 	<Accordion title="Accordion title">
@@ -12,4 +12,43 @@ export const CautionAccordion = () => (
 		Our monthly newsletter, keeping you up to date with{" "}
 		<a href="#">important developments at NICE</a>
 	</Accordion>
+);
+
+export const AccordionGroupExample = () => (
+	<AccordionGroup>
+		<Accordion key="1" title="Accordion 1">
+			Accordion 1 body
+		</Accordion>
+		<Accordion key="2" title="Accordion 2">
+			Accordion 2 body
+		</Accordion>
+	</AccordionGroup>
+);
+
+export const AccordionGroupWithCautionExample = () => (
+	<AccordionGroup>
+		<Accordion key="1" title="Accordion 1" isCaution={true}>
+			Caution Accordion 1 body
+		</Accordion>
+		<Accordion key="2" title="Accordion 2">
+			Accordion 2 body
+			<Accordion key="2" title="Nested Accordion 1">
+				Nested Accordion 1 body
+			</Accordion>
+		</Accordion>
+	</AccordionGroup>
+);
+
+export const AccordionGroupNestedAccordionExample = () => (
+	<AccordionGroup>
+		<Accordion key="1" title="Accordion 1">
+			Accordion 1 body
+		</Accordion>
+		<Accordion key="2" title="Accordion 2">
+			Accordion 2 body
+			<Accordion key="2" title="Nested Accordion 1">
+				Nested Accordion 1 body
+			</Accordion>
+		</Accordion>
+	</AccordionGroup>
 );
