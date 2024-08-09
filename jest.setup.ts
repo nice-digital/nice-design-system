266d099a6d9c618456a696a6bee3d7e866d349da
Dale H <dale.hudson@nice.org.uk>
2024-08-09 18:14:14 +0100

    NDS-426: (chore) update jest setup
    - include TextEncoder, TextDecoder global for testing Accordion server
      renders

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,3 +1,16 @@
 import "@testing-library/jest-dom";
 import "@testing-library/jest-dom/extend-expect";
 import "jest-extended";
+
+/* NOTE: import and global setup for TextEncoder/TextDecoder
+	workaround for error:
+	ReferenceError: TextEncoder is not defined in isUseClient
+	https://github.com/testing-library/dom-testing-library/issues/1245
+	https://github.com/jsdom/jsdom/issues/2524#issuecomment-897707183
+
+	related to useIsClient, Accordion and Accordiongroup tests
+
+*/
+import { TextEncoder, TextDecoder as NodeTextDecoder } from "util";
+global.TextEncoder = TextEncoder;
+global.TextDecoder = NodeTextDecoder as typeof TextDecoder;
