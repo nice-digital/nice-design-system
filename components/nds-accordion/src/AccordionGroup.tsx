0133bada7728fdb2ed76c72c4946f908d431137c
Shilpa Unni <shilpa.unni@nice.org.uk>
2025-08-08 12:03:42 +0100

    NDS-473 Add classname in BEM format

--- components/nds-accordion/src/AccordionGroup.tsx
+++ components/nds-accordion/src/AccordionGroup.tsx
@@ -48,7 +48,7 @@ export const AccordionGroup: FC<AccordionGroupProps> = ({
 					<Toggle isOpen={isGroupOpen}>{toggleText(isGroupOpen)}</Toggle>
 				</button>
 			) : null}
-			<div data-id="accordion-group-content">{children}</div>
+			<div className="accordionGroup__content">{children}</div>
 		</AccordionGroupProvider>
 	);
 };
