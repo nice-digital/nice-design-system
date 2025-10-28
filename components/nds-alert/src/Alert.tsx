6cc1eb34d78c9f0acb525ffd2ff1a828c669ea01
Becca Dawson <rebecca.dawson@nice.org.uk>
2025-10-28 17:16:02 +0000

    NDS-486 allow overwiting of alert role on Alert component for less critical alerts

--- components/nds-alert/src/Alert.tsx
+++ components/nds-alert/src/Alert.tsx
@@ -8,23 +8,28 @@ const allowedTypes = ["info", "caution", "error", "success"];
 interface AlertProps {
 	[prop: string]: unknown;
 	type?: "info" | "caution" | "error" | "success";
+	nonIntrusive?: boolean;
 	children: React.ReactNode;
 }
 
 export const Alert: React.FC<AlertProps> = ({
 	children,
 	type = "info",
+	nonIntrusive = false,
 	...rest
 }: AlertProps) => {
 	const classNames = classnames({
 		alert: true,
 		[`alert--${type}`]: true
 	});
+	const ariaBehaviour = nonIntrusive
+		? { "aria-live": "polite" as "polite" }
+		: { role: "alert" };
 	return (
 		<div
 			className={classNames}
 			data-component={`alert${type ? `--${type}` : ""}`}
-			role="alert"
+			{...ariaBehaviour}
 			{...rest}
 		>
 			{children}
