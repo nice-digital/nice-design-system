77725d68b11eeee35ffc98988ffc6755955ebd38
Shilpa Unni <shilpa.unni@nice.org.uk>
2025-12-11 16:12:12 +0000

    NDS-493 Added name attribute back to Form group -Removed name attribute from individual radios

--- docs/components/examples/Radio.tsx
+++ docs/components/examples/Radio.tsx
@@ -2,35 +2,43 @@ import { FormGroup } from "@nice-digital/nds-form-group";
 import { Radio } from "@nice-digital/nds-radio";
 
 export const DefaultRadio = () => (
-	<FormGroup legend="Are you happy for us to contact you in the future?">
-		<Radio label="Yes" value="yes" name="defaultradio" />
-		<Radio label="No" value="no" name="defaultradio" />
+	<FormGroup
+		legend="Are you happy for us to contact you in the future?"
+		name="contact-preference-default"
+	>
+		<Radio label="Yes" value="yes" />
+		<Radio label="No" value="no" />
 	</FormGroup>
 );
 
 export const DefaultRadioExample = () => (
-	<FormGroup legend="Are you happy for us to contact you in the future?">
-		<Radio label="Yes" value="yes" name="defaultradioexample" />
-		<Radio label="No" value="no" name="defaultradioexample" />
+	<FormGroup
+		legend="Are you happy for us to contact you in the future?"
+		name="contact-preference-default-example"
+	>
+		<Radio label="Yes" value="yes" />
+		<Radio label="No" value="no" />
 	</FormGroup>
 );
 
 export const InlineRadio = () => (
-	<FormGroup legend="Are you happy for us to contact you in the future?" inline>
-		<Radio inline label="Yes" value="yes" name="inlineradio" />
-		<Radio inline label="No" value="no" name="inlineradio" />
+	<FormGroup
+		legend="Are you happy for us to contact you in the future?"
+		inline
+		name="contact-preference-inline"
+	>
+		<Radio label="Yes" value="yes" />
+		<Radio label="No" value="no" />
 	</FormGroup>
 );
 
 export const HintRadio = () => (
-	<FormGroup legend="Are you happy for us to contact you in the future?">
-		<Radio
-			hint="Some helpful hint text"
-			label="Yes"
-			value="yes"
-			name="hintradio"
-		/>
-		<Radio label="No" value="no" name="hintradio" />
+	<FormGroup
+		legend="Are you happy for us to contact you in the future?"
+		name="contact-preference-hint"
+	>
+		<Radio label="Yes" value="yes" hint="Some helpful hint text" />
+		<Radio label="No" value="no" />
 	</FormGroup>
 );
 
@@ -38,20 +46,19 @@ export const GroupHintRadio = () => (
 	<FormGroup
 		legend="Are you happy for us to contact you in the future?"
 		hint="Some helpful hint text"
+		name="contact-preference-grouphint"
 	>
-		<Radio label="Yes" value="yes" name="grouphintradio" />
-		<Radio label="No" value="no" name="grouphintradio" />
+		<Radio label="Yes" value="yes" />
+		<Radio label="No" value="no" />
 	</FormGroup>
 );
 
 export const ErrorHintRadio = () => (
-	<FormGroup legend="Are you happy for us to contact you in the future?">
-		<Radio
-			error="Error message"
-			label="Yes"
-			value="yes"
-			name="errorhintradio"
-		/>
-		<Radio label="No" value="no" name="errorhintradio" />
+	<FormGroup
+		legend="Are you happy for us to contact you in the future?"
+		name="contact-preference-errorhint"
+	>
+		<Radio label="Yes" value="yes" error="Error message" />
+		<Radio label="No" value="no" />
 	</FormGroup>
 );
