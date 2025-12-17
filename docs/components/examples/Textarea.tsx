eb9757d1b99a7ccff1874b2922c332424ed8a9b5
Shilpa Unni <shilpa.unni@nice.org.uk>
2025-12-17 14:39:32 +0000

    NDS-496 Update textarea documentation

--- docs/components/examples/Textarea.tsx
+++ docs/components/examples/Textarea.tsx
@@ -1,10 +1,11 @@
+import { FormGroup } from "@nice-digital/nds-form-group";
 import { Textarea } from "@nice-digital/nds-textarea";
 
-export const DefaultTextarea = () => (
+export const DefaultTextareaExample = () => (
 	<Textarea label="Your address" name="address"></Textarea>
 );
 
-export const HintTextarea = () => (
+export const SingleHintTextarea = () => (
 	<Textarea
 		label="Your address"
 		name="addresshint"
@@ -12,11 +13,34 @@ export const HintTextarea = () => (
 	></Textarea>
 );
 
-export const ErrorTextarea = () => (
+export const SingleErrorTextarea = () => (
 	<Textarea
 		label="Your address"
 		name="addresshint"
 		error={true}
 		errorMessage="Please enter your full address"
 	></Textarea>
+);
+
+export const GroupedTextarea = () => (
+	<FormGroup legend="Feedback Details">
+		<Textarea
+			label="What could be improved?"
+			name="improvement_feedback"
+		></Textarea>
+		<Textarea label="Additional Comments" name="additional_comments"></Textarea>
+	</FormGroup>
+);
+
+export const GroupedHintTextarea = () => (
+	<FormGroup
+		legend="Feedback Details"
+		hint="Please answer all fields clearly and avoid including any personal or sensitive information."
+	>
+		<Textarea
+			label="What could be improved?"
+			name="improvement_feedback"
+		></Textarea>
+		<Textarea label="Additional Comments" name="additional_comments"></Textarea>
+	</FormGroup>
 );
