759c02886bce547355e9746e599fdd53c2d0e7b6
Dale Hudson <dalefrontend@gmail.com>
2026-01-16 14:35:40 +0000

    NDS-497 update text input component documentation (#391)
    * NDS-497 update text content of text input documentation
    
    TODO:
    - create new examples for grouped inputs and add to docs
    
    * NDS-497 add grouped text input examples
    
    * NDS-497 update text input docs with group examples
    
    * NDS-497 update heading levels
    
    * NDS-497 update heading levels and text
    
    * NDS-497 update HTML example code with id and name attributes
    
    * NDS-497 update grouped input examples
    
    * NDS-497 update sepearate example to avoid duped ids
    
    * NDS-497 replace brackets with dashes for WCAG guidline references
    
    * NDS-511 comment error style content with TODO to add back once new error styles are implemented
    
    * NDS-497 update type fixes on text input docs
    
    * NDS-497 fix typos and update entities
    
    * NDS-497 typo fix
    
    * NDS-497 update wording from doc

--- docs/components/examples/Input.tsx
+++ docs/components/examples/Input.tsx
@@ -1,6 +1,12 @@
 import { Input } from "@nice-digital/nds-input";
+import { FormGroup } from "@nice-digital/nds-form-group";
+
+export const DefaultInputExample = () => (
+	<Input label="First name" name="firstname-example" />
+);
 
 export const DefaultInput = () => <Input label="First name" name="firstname" />;
+
 export const ErrorInput = () => (
 	<Input
 		label="Surname"
@@ -10,6 +16,27 @@ export const ErrorInput = () => (
 		errorMessage="Please enter your surname"
 	/>
 );
+
+export const GroupedInputs = () => (
+	<FormGroup legend="What is your address">
+		<Input label="Address line 1" name="address-line-1" />
+		<Input label="Address line 2" name="address-line-2" />
+		<Input label="Town or city" name="address-town" />
+		<Input label="County" name="address-county" />
+		<Input label="Postcode" name="address-postcode" />
+	</FormGroup>
+);
+
 export const HintInput = () => (
 	<Input label="Age" name="age" hint="Please enter in years" />
+);
+
+export const GroupedInputHints = () => (
+	<FormGroup legend="What is your address" hint="This should be a UK address">
+		<Input label="Address line 1" name="address-line-1-hint" />
+		<Input label="Address line 2" name="address-line-2-hint" />
+		<Input label="Town or city" name="address-town-hint" />
+		<Input label="County" name="address-county-hint" />
+		<Input label="Postcode" name="address-postcode-hint" />
+	</FormGroup>
 );
